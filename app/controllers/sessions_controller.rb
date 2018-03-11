ef13a47bd8a2e1c3456292ce08c876f8439edde4
Alvin Situ <jisitu@ucdavis.edu>
2018-03-11 00:36:22 -0800

    add session files

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create # create new session i.e. log-in a user
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log in user and redirect to the user's profile.
+      log_in user
+      redirect_to user
+    else
+      # Create an error message.
+      flash.now[:danger] = "Invalid User or Password."
+      render 'new'
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url    
+  end
+end
